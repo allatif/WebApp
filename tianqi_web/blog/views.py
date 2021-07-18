2a144576e3497399389e75b036d78d4c4c849aaa
Omar Al-Latif <omar.al-latif@hochschule-stralsund.de>
2021-07-18 14:15:54 +0200

    Database and Migrations for User and Post / updated Admin

--- tianqi_web/blog/views.py
+++ tianqi_web/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'tiVnqi',
-        'title' : 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 11, 2021'
-    },
-    {
-        'author': 'Max Mustermann',
-        'title' : 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 12, 2021'
-    }
-]
+from . models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
